6e1334c5dbe53dfdd214c9404b4a8a149522515d
jjthelege <joaonetotfm@gmail.com>
2022-01-16 20:38:54 -0300

    initializing crud

--- index.js
+++ index.js
@@ -16,6 +16,43 @@ app.engine('handlebars', exphbs.engine())
 
 app.set('view engine', 'handlebars')
 
+app.get('/registrando', (req, res) => {
+    res.render('form')
+})
+
+//inserindo os dados no banco local
+app.post('/registrar-horas', (req, res) => {
+    const disciplina = req.body.disciplina
+    const horas = req.body.horas
+    const descricao = req.body.descricao
+    const data = req.body.data
+
+    const query = `INSERT INTO study (disciplina, data, descricao, horas) VALUES ('${disciplina}', '${data}','${descricao}','${horas}')`
+
+    conn.query(query, (err) => {
+        if (err) {
+            console.log(err);
+        }
+
+    })
+
+    res.redirect('/registrando')
+})
+
+//retornando os dados armazenados no banco local
+
+app.get('/registros', (req, res) => {
+    const query = `SELECT * FROM study`
+
+    conn.query(query, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        const dados = data
+        console.log(dados.id);
+        res.render('data', { dados })
+    })
+})
 
 //primeiro end-point
 app.get('/', (req, res) => {
